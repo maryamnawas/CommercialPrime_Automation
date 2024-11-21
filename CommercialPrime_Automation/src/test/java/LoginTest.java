2e02270d08c32848d623531f0547fc69d8a07610
maryamnawas <125673565+maryamnawas@users.noreply.github.com>
2024-11-21 12:03:20 +0530

    Enter login details and go to dashboard

---
+++ CommercialPrime_Automation/src/test/java/LoginTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void OpenLinkTestPage(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://play.primeq.co/");
+    }
+
+    @Test
+    public void LoginDetails(){
+        // Locate and fill in the username
+        WebElement usernameField = driver.findElement(By.xpath("//input[@placeholder='Username or Email']"));
+        usernameField.sendKeys("Marii");
+
+        // Locate and fill in the password
+        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        passwordField.sendKeys("1234");
+
+        // Locate and click the login button
+        WebElement loginButton = driver.findElement(By.className("LoginForm_LoginButton__xSMas"));
+        loginButton.click();
+    }
+}
