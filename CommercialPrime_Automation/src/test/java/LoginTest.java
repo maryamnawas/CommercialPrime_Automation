e9849023775cab65a5edc5caaa60b1d89abfe641
maryamnawas <125673565+maryamnawas@users.noreply.github.com>
2024-11-29 13:28:21 +0530

    done with highprivacy  mode testing

--- CommercialPrime_Automation/src/test/java/LoginTest.java
+++ CommercialPrime_Automation/src/test/java/LoginTest.java
@@ -2,23 +2,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.Objects;
 
 public class LoginTest {
     WebDriver driver;
 
     @BeforeMethod
-    public void OpenLinkTestPage(){
+    public void LoginDetails() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://play.primeq.co/");
-    }
 
-    @Test
-    public void LoginDetails(){
         // Locate and fill in the username
         WebElement usernameField = driver.findElement(By.xpath("//input[@placeholder='Username or Email']"));
         usernameField.sendKeys("Marii");
@@ -32,11 +32,34 @@ public class LoginTest {
         loginButton.click();
 
         // Validate login (check for successful navigation or specific element)
-        String expectedUrl = "https://play.primeq.co/home";
-        if (Objects.requireNonNull(driver.getCurrentUrl()).equals(expectedUrl)) {
-            System.out.println("Login failed.");
-        } else {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://play.primeq.co/home"));
+
+        if (Objects.equals(driver.getCurrentUrl(), "https://play.primeq.co/home")) {
             System.out.println("Login successful!");
+        } else {
+            System.out.println("Login failed.");
+        }
+    }
+
+    @Test
+    public void HighPrivacyCheck() {
+        // Wait until the High Privacy toggle switch is visible
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement toggleSwitch = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("div[class='Home_HighPrivacySwitch__1ghaJ'] button[role='switch']")
+        ));
+
+        // Check the value of the "aria-checked" attribute
+        String isChecked = toggleSwitch.getAttribute("aria-checked");
+
+        // Validate the state of the High Privacy Mode
+        if ("true".equals(isChecked)) {
+            System.out.println("High Privacy Mode is ON.");
+        } else if ("false".equals(isChecked)) {
+            System.out.println("High Privacy Mode is OFF.");
+        } else {
+            System.out.println("Unable to determine the state of High Privacy Mode.");
         }
     }
 }
